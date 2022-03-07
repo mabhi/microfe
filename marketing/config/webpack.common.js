afa2a122ace1d58a4970ae0798eac929c755dd00
Abhijit M <mukherjeeab@gmail.com>
2022-03-07 21:08:26 +0530

    webpack config removed html plugin from common and added to dev only

--- marketing/config/webpack.common.js
+++ marketing/config/webpack.common.js
@@ -1,5 +1,3 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
   module: {
     rules: [
@@ -18,9 +16,4 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
 };
